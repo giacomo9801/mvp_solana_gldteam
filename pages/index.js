4b113ffd34e8b65d29b5456bf41794af2ca31b58
giacomo <giacomocorcella17@gmail.com>
2024-05-31 17:26:16 +0200

    NextJS Solana MVP Init

--- pages/index.js
+++ pages/index.js
@@ -1,123 +1,246 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import React, { useState } from "react";
+import Image from "next/image";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createNft,
+  mplTokenMetadata,
+  fetchAllDigitalAssetByOwner,
+} from "@metaplex-foundation/mpl-token-metadata";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import {
+  publicKey,
+  generateSigner,
+  percentAmount,
+} from "@metaplex-foundation/umi";
+import { base58 } from "@metaplex-foundation/umi/serializers";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import wallet from "./wallet.json";
+import { Stepper, Step, StepLabel, Button, TextField, Typography } from "@mui/material";
 
-const inter = Inter({ subsets: ['latin'] })
+const connectionUrl = "https://api.devnet.solana.com";
+const commitment = "finalized";
 
-export default function Home() {
+function App() {
+  const [activeStep, setActiveStep] = useState(0);
+  const [uri, setUri] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [uploadResult, setUploadResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [metadataValues, setMetadataValues] = useState({
+    title: "",
+    object: "",
+    description: "",
+    date: "",
+  });
+
+  const steps = ["Upload Image", "Enter Metadata", "Mint NFT"];
+
+  async function initializeUmi(image) {
+    const umi = createUmi(connectionUrl, commitment);
+    umi.use(irysUploader());
+
+    const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+    const myKeypairSigner = createSignerFromKeypair(umi, keypair);
+    umi.use(signerIdentity(myKeypairSigner));
+
+    const nftImage = createGenericFile(image, "Document");
+
+    const [myUri] = await umi.uploader.upload([nftImage]);
+    return myUri;
+  }
+
+  const uploadImage = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const image = await readFile(selectedImage);
+      const myUri = await initializeUmi(image);
+      setUploadResult(myUri);
+      console.log("Image upload successful:", myUri);
+      const assets = await fetchAllDigitalAssetByOwner(umi2, "CdZQgtkT8actnoUpjPPvadKWvSw4gYZ9w9SQeeqY5y6k");
+      console.log(assets);
+
+      handleNext();
+    } catch (err) {
+      setError("Error uploading image: " + err.message);
+      console.error("Error uploading image:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const createMetadata = async () => {
+    try {
+      const umi = createUmi(connectionUrl, commitment);
+      umi.use(irysUploader());
+      const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+      const myKeypairSigner = createSignerFromKeypair(umi, keypair);
+      umi.use(signerIdentity(myKeypairSigner));
+
+      const metadata = {
+        name: "NFTGLD",
+        symbol: "MVPGLD",
+        description: metadataValues.description,
+        image: uploadResult,
+        attributes: [
+          { trait_type: "Object", value: metadataValues.object },
+          { trait_type: "Date", value: metadataValues.date },
+          { trait_type: "Description", value: metadataValues.description },
+          { trait_type: "Title", value: metadataValues.title },
+        ],
+        properties: {
+          files: [{ type: "image/jpeg", uri: uploadResult }],
+        },
+      };
+
+      const nftUri = await umi.uploader.uploadJson(metadata);
+      setUri(nftUri);
+      console.log("Metadata upload successful:", nftUri);
+      handleNext();
+    } catch (err) {
+      console.error("Error uploading metadata:", err);
+    }
+  };
+
+  const umi2 = createUmi("https://api.devnet.solana.com", "finalized");
+  let keypair2 = umi2.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+  const myKeypairSigner2 = createSignerFromKeypair(umi2, keypair2);
+  umi2.use(signerIdentity(myKeypairSigner2)).use(mplTokenMetadata());
+  const mint = generateSigner(umi2);
+  const sellerFeeBasisPoints = percentAmount(5, 2);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setMetadataValues({ ...metadataValues, [name]: value });
+  };
+
+  const name = "NFTGLD";
+
+  const mintNFT = async () => {
+    let tx = createNft(umi2, {
+      symbol: "MVPGLD",
+      mint,
+      name,
+      uri,
+      sellerFeeBasisPoints,
+    });
+
+    let result = await tx.sendAndConfirm(umi2);
+    const signature = base58.deserialize(result.signature);
+    console.log(signature);
+    console.log("NFT minted successfully:", result);
+    handleNext();
+  };
+
+  const readFile = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsArrayBuffer(file);
+    });
+  };
+
+  const handleNext = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const handleBack = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
   return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
+    <div className="App">
+      <header className="App-header">
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        {activeStep === 0 && (
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+            <label htmlFor="imageInput">Select NFT image:</label>
+            <input id="imageInput" type="file" accept="image/*" onChange={handleImageChange} />
+            {selectedImage && (
+              <div className="image-preview">
+                <Image
+                  src={URL.createObjectURL(selectedImage)}
+                  alt="Preview"
+                  width={200}
+                  height={200}
+                />
+              </div>
+            )}
+            <Button onClick={uploadImage} disabled={loading}>
+              {loading ? "Uploading..." : "Upload NFT Image"}
+            </Button>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
+        )}
+        {activeStep === 1 && uploadResult && (
+          <div>
+            <TextField
+              label="Title"
+              name="title"
+              value={metadataValues.title}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
             />
+            <TextField
+              label="Object"
+              name="object"
+              value={metadataValues.object}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Description"
+              name="description"
+              value={metadataValues.description}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Date"
+              name="date"
+              type="date"
+              value={metadataValues.date}
+              onChange={handleInputChange}
+              InputLabelProps={{ shrink: true }}
+              fullWidth
+              margin="normal"
+            />
+            <Button onClick={createMetadata} disabled={loading}>
+              {loading ? "Uploading..." : "Create Metadata"}
+            </Button>
           </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </>
-  )
+        )}
+        {activeStep === 2 && uri && (
+          <div>
+            <Typography variant="h6">Ready to Mint your NFT!</Typography>
+            <Button onClick={mintNFT} disabled={loading}>
+              {loading ? "Minting..." : "Mint NFT"}
+            </Button>
+          </div>
+        )}
+        {activeStep < steps.length - 1 && (
+          <Button disabled={activeStep === 0} onClick={handleBack}>
+            Back
+          </Button>
+        )}
+        {error && <Typography color="error">{error}</Typography>}
+      </header>
+    </div>
+  );
 }
+
+export default App;
